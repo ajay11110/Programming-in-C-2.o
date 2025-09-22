0d6eb61e19c01654b6be7753ef2c8375373c7114
Ajay Yadav <ayajay2006@gmail.com>
2025-09-22 22:29:27 +0530

    Add files via upload

---
+++ equilateral triangle pattern.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+int a;
+char b;
+
+printf("enter lines :");
+scanf("%d", &a);
+
+printf("enter symbol :");
+scanf(" %c", &b);
+
+for (int i=1;i<=a;i++)
+{
+for (int j=0;j<a-i;j++)
+{
+printf(" ");
+}
+
+for (int k=0;k<2*i-1;k++)
+{
+printf("%c",b);
+}
+
+printf("\n");
+}
+
+return 0;
+}
+
+    
