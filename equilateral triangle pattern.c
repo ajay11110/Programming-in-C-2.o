44c29d6796777dc20cd1000ce317e6a2d3fabd9f
Ajay Yadav <ayajay2006@gmail.com>
2025-10-02 22:38:03 +0530

    Update equilateral triangle pattern.c

--- equilateral triangle pattern.c
+++ equilateral triangle pattern.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
-int main() {
-int a;
-char b;
-
-printf("enter lines :");
-scanf("%d", &a);
-
-printf("enter symbol :");
-scanf(" %c", &b);
-
-for (int i=1;i<=a;i++)
+int main()
 {
-for (int j=0;j<a-i;j++)
-{
-printf(" ");
-}
+    int a;
+    char b;
 
-for (int k=0;k<2*i-1;k++)
-{
-printf("%c",b);
-}
+    printf("enter lines :");
+    scanf("%d", &a);
 
-printf("\n");
-}
+    printf("enter symbol :");
+    scanf(" %c", &b);
 
-return 0;
-}
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 0; j < a - i; j++)
+        {
+            printf(" ");
+        }
 
-    
+        for (int k = 0; k < 2 * i - 1; k++)
+        {
+            printf("%c", b);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
